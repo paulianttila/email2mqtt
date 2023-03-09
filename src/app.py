f03ab00b6662288040def8d82b4a6b9f94610a24
Pauli Anttila <pauli.anttila@gmail.com>
2023-03-09 16:21:49 +0200

    Refactoring

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from email.message import Message
+import os
 from mqtt_framework import Framework
 from mqtt_framework import Config
 from mqtt_framework.callbacks import Callbacks
@@ -6,7 +8,6 @@ from mqtt_framework.app import TriggerSource
 from prometheus_client import Counter
 
 from datetime import datetime
-import os
 import threading
 import time
 import email
@@ -73,100 +74,38 @@ class MyApp:
         return self.email_reader.is_alive()
 
     def do_update(self, trigger_source: TriggerSource) -> None:
-        self.logger.debug("update called, trigger_source=%s", trigger_source)
+        self.logger.debug(f"Update called, trigger_source={trigger_source}")
         if self.email_reader is None:
             self.logger.info("Start email client")
             self.email_reader = threading.Thread(target=self.start, daemon=True)
             self.email_reader.start()
 
-    def process_email(self, mail):
-        from_who = mail["from"]
-        subject = mail["subject"]
-        timestamp = email.utils.parsedate_to_datetime(mail["date"])
-        timestamp = str(
-            timestamp.replace(microsecond=0)
-            .astimezone(pytz.timezone("Europe/Helsinki"))
-            .isoformat()
-        )
-        body = None
-        charset = None
-        message = None
-
-        if mail.is_multipart():
-            for part in mail.walk():
-                ctype = part.get_content_type()
-                cdispo = str(part.get("Content-Disposition"))
-
-                # skip any text/plain (txt) attachments
-                if ctype == "text/plain" and "attachment" not in cdispo:
-                    body = part.get_payload(decode=True)
-                    charset = part.get_content_charset()
-                    break
-        else:
-            body = mail.get_payload(decode=True)
-            charset = mail.get_content_charset()
-
-        if body is not None:
-            self.logger.debug('body: "%s"', body)
-            self.logger.debug("charset: %s", charset)
-
-            if charset is not None:
-                message = str(body, charset)
-            else:
-                message = str(body)
-
-        self.logger.debug("%s: %s - %s - %s", timestamp, from_who, subject, message)
-
-        m = {
-            "from": from_who,
-            "subject": subject,
-            "date": timestamp,
-            "received": str(datetime.now().replace(microsecond=0).isoformat()),
-            "message": message,
-        }
-
-        t = json.dumps(m)
-        self.logger.info("%s", t)
-        self.publish_value_to_mqtt_topic("message", t, True)
-
-    def receive_emails(self):
-        idle_timeout = int(self.config["EMAIL_IDLE_TIMEOUT"])
+    def start(self) -> None:
+        while not self.exit:
+            try:
+                if self.imap is None:
+                    self.login()
+                self.wait_emails()
+            except Exception as e:
+                self.received_emails_errors_metric.inc()
+                self.logger.error(f"Error occured: {e}")
+                self.logger.debug(f"Error occured: {e}", exc_info=True)
+                self.imap = None
+                time.sleep(10)
+            time.sleep(1)
 
         try:
-            while not self.exit:
-                self.logger.debug("waiting new emails %ds", idle_timeout)
-                self.imap.idle()
-                result = self.imap.idle_check(timeout=idle_timeout)
-                self.logger.debug("waiting end")
-                self.imap.idle_done()
-                self.imap.noop()
-
-                if len(result) > 0:
-                    messages = self.imap.search("UNSEEN")
-                    self.logger.debug("received %d email(s)", len(messages))
-                    for uid, message_data in self.imap.fetch(
-                        messages, "RFC822"
-                    ).items():
-                        self.received_emails_metric.inc()
-                        email_message = email.message_from_bytes(
-                            message_data[b"RFC822"]
-                        )
-                        self.logger.info(
-                            "processing email %s from %s", uid, email_message["from"]
-                        )
-                        self.process_email(email_message)
-                else:
-                    self.logger.debug("no new messages seen")
+            self.imap.logout()
         finally:
-            self.imap.idle_done()
+            self.logger.debug("Email reader stopped")
 
-    def login(self):
+    def login(self) -> None:
         self.logger.info("Connecting to IMAP server %s", self.config["EMAIL_SERVER"])
         self.imap = IMAPClient(self.config["EMAIL_SERVER"], use_uid=True, ssl=True)
         self.logger.info(
             "Logging in to IMAP server by user %s", self.config["EMAIL_USERNAME"]
         )
-        result = self.imap.login(
+        self.imap.login(
             self.config["EMAIL_USERNAME"], self.config["EMAIL_PASSWORD"]
         )
         self.logger.info("Selecting IMAP folder - %s", self.config["EMAIL_FOLDER"])
@@ -176,27 +115,103 @@ class MyApp:
             self.logger.info("Skip %d unread messages", len(messages))
             self.imap.fetch(messages, "RFC822")
 
-    def start(self):
+    def wait_emails(self) -> None:
+        idle_timeout = int(self.config["EMAIL_IDLE_TIMEOUT"])
         while not self.exit:
             try:
-                if self.imap is None:
-                    self.login()
+                self.wait_emails_with_timeout(idle_timeout)
+            finally:
+                self.imap.idle_done()
 
-                self.receive_emails()
-            except Exception as e:
-                self.received_emails_errors_metric.inc()
-                self.logger.error("Error occured: %s" % e)
-                self.logger.debug("Error occured: %s" % e, exc_info=True)
-                self.imap = None
-                time.sleep(10)
+    def wait_emails_with_timeout(self, idle_timeout):
+        self.logger.debug(f"Waiting new emails {idle_timeout} sec")
+        self.imap.idle()
+        result = self.imap.idle_check(timeout=idle_timeout)
+        if len(result) > 0:
+            self.check_new_emails()
+        else:
+            self.logger.debug("No new messages seen")
+        self.logger.debug("Waiting end")
+        self.imap.idle_done()
+        self.imap.noop()
 
-            time.sleep(1)
+    def check_new_emails(self) -> None:
+        messages = self.imap.search("UNSEEN")
+        self.logger.debug(f"Received {len(messages)} email(s)")
+        for uid, message_data in self.imap.fetch(
+            messages, "RFC822"
+        ).items():
+            self.received_emails_metric.inc()
+            email_message = email.message_from_bytes(
+                message_data[b"RFC822"]
+            )
+            self.logger.info(
+                "Processing email %s from %s", uid, email_message["from"]
+            )
+            self.process_email(email_message)
 
-        try:
-            self.imap.logout()
-        finally:
-            self.logger.debug("Email reader stopped")
+    def process_email(self, mail: Message) -> None:
+        from_who = mail["from"]
+        subject = mail["subject"]
+        timestamp = self.get_email_date_as_str(mail)
+        message = self.get_message(mail)
+        now = datetime.now().replace(microsecond=0).isoformat()
 
+        self.logger.debug("%s: %s - %s - %s", timestamp, from_who, subject, message)
+
+        msg = {
+            "from": from_who,
+            "subject": subject,
+            "date": timestamp,
+            "received": now,
+            "message": message,
+        }
+
+        data = json.dumps(msg)
+        self.logger.info(f"{data}")
+        self.publish_value_to_mqtt_topic("message", data, True)
+
+    def get_email_date_as_str(self, mail: Message) -> str:
+        timestamp = email.utils.parsedate_to_datetime(mail["date"])
+        return str(
+            timestamp.replace(microsecond=0)
+            .astimezone(pytz.timezone(os.environ['TZ']))
+            .isoformat()
+        )
+
+    def get_message(self, mail: Message) -> None | str:
+        body = None
+        charset = None
+
+        if mail.is_multipart():
+            body, charset = self.get_multipart_email_body(mail)
+        else:
+            body, charset = self.get_email_body(mail)
+
+        if body is not None:
+            self.logger.debug(f"body: {body}")
+            self.logger.debug(f"charset: {charset}")
+
+            return str(body, charset) if charset is not None else str(body)
+
+    def get_multipart_email_body(self, mail: Message) -> tuple[str, str]:
+        body = None
+        charset = None
+
+        for part in mail.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get("Content-Disposition"))
+
+            # skip any text/plain (txt) attachments
+            if ctype == "text/plain" and "attachment" not in cdispo:
+                body = part.get_payload(decode=True)
+                charset = part.get_content_charset()
+        return body, charset
+
+    def get_email_body(self, mail: Message) -> tuple[str, str]:
+        body = mail.get_payload(decode=True)
+        charset = mail.get_content_charset()
+        return body, charset
 
 if __name__ == "__main__":
     Framework().start(MyApp(), MyConfig(), blocked=True)
